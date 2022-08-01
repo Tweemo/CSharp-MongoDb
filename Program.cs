8b63d5d7727e33f09fe14d1a2b4bb6ac2a1cc217
Tweemo <timothyliew88@hotmail.com>
2022-08-01 18:33:46 +1200

    insert, filter and update entries in mongo

--- Program.cs
+++ Program.cs
@@ -2,3 +2,30 @@
 
 MongoClient client = new MongoClient("mongodb+srv://tim:mongodb@testcluster.cbjs6.mongodb.net/?retryWrites=true&w=majority");
 
+
+// Create a new collection called "playlists" in the "test" database.
+var playlistCollection = client.GetDatabase("sample_mflix").GetCollection<Playlist>("playlist");
+
+// Create an object movieList that is to be inserted the items in the playlist.
+List<string> movieList = new List<string>();
+movieList.Add("movie1");
+playlistCollection.InsertOne(new Playlist("yaboi", movieList ));
+
+// Filters the playlist collection to find the playlist with the username "yaboi".
+FilterDefinition<Playlist> filter = Builders<Playlist>.Filter.Eq("username", "yaboi");
+List<Playlist> results = playlistCollection.Find(filter).ToList();
+
+// Prints the items under the username yaboi
+foreach (Playlist result in results) {
+  Console.WriteLine(string.Join(", ", result.items));
+}
+
+// Updates 'items' in the playlist collection to add 'movie2' to the list to the playlist with the username 'yaboi'.
+UpdateDefinition<Playlist> update = Builders<Playlist>.Update.AddToSet<string>("items", "movie2");
+playlistCollection.UpdateOne(filter, update);
+
+// Printers the updated playlist.
+results = playlistCollection.Find(filter).ToList();
+foreach (Playlist result in results) {
+  Console.WriteLine(string.Join(", ", result.items));
+}
---
+++ playlist.cs
@@ -0,0 +1,13 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+public class Playlist {
+
+  public ObjectId _id { get; set; }
+  public string username { get; set; } = null!;
+  public List<string> items { get; set; } = null!;
+  public Playlist(string username, List<string> movieIds) {
+    this.username = username;
+    this.items = movieIds;
+  }
+}
